d919d28a584712b1c74f3daa3fa289888930a3d1
chenpc <601059114@qq.com>
2021-03-23 16:25:21 +0800

    2021年3月10日17:06:59

--- src/test/java/com/chen/admin/DemoWebAdminApplicationTests.java
+++ src/test/java/com/chen/admin/DemoWebAdminApplicationTests.java
@@ -1,18 +1,35 @@
 package com.chen.admin;
 
+import com.chen.admin.bean.User;
 import org.junit.jupiter.api.Test;
+import org.openjdk.jol.info.ClassLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
 @SpringBootTest
 class DemoWebAdminApplicationTests {
+
+    public static void main(String[] args) {
+        User u =new User();
+        String s = ClassLayout.parseInstance(u).toPrintable().toString();
+        System.out.println(s);
+    }
+
     @Autowired
     JdbcTemplate jdbcTemplate;
     @Autowired
     DataSource dataSource;
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
     @Test
     void contextLoads() {
         System.out.println(dataSource.getClass());
@@ -22,6 +39,20 @@ class DemoWebAdminApplicationTests {
             System.out.println(map.get("id"));
             System.out.println(map.get("username"));
         }*/
+    }
+    @Test
+    void redis(){
+//        Object name = redisTemplate.opsForValue().get("name");
+//        Object chen = redisTemplate.opsForValue().get("chen");
+//        System.out.println(name);
+//
+//        System.out.println(chen);
+       // stringRedisTemplate.opsForValue().set("aaaa", "aaaa");
+
+      //  String name = stringRedisTemplate.opsForValue().get("name");
+     //   System.out.println(name);
+
+
     }
 
 }
